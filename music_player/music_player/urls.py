9e1de3cf5f2c784c6c7f9eac749d94c049d1d2fa
Dariiiaaa <159879727+Dariiiaaa@users.noreply.github.com>
2024-04-24 17:12:54 +0300

    Update urls.py

--- music_player/music_player/urls.py
+++ music_player/music_player/urls.py
@@ -1,7 +1,13 @@
+# music_player/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload_songs/', views.upload_songs, name='upload_songs'),
+    path('play/<str:song>/', views.play, name='play'),
+    path('pause/', views.pause, name='pause'),
+    path('stop/', views.stop, name='stop'),
+    path('resume/', views.resume, name='resume'),
+   
 ]
